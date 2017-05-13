571d573dd77b599049ac3044f48c589393d31593
David Sorokin <david.sorokin@gmail.com>
2017-05-13 07:04:15 +0300

    Using Int64 for identifiers.

--- Simulation/Aivika/Experiment/Entity/UUID.hs
+++ Simulation/Aivika/Experiment/Entity/UUID.hs
@@ -16,37 +16,43 @@ module Simulation.Aivika.Experiment.Entity.UUID
 
 import Numeric
 
+import Data.Int
+
 import System.Random
 
 import Simulation.Aivika
 
 -- | The unique identifier.
-type UUID = String
+type UUID = Int64
 
 -- | Generate an unique identifier.
 newRandomUUID :: IO UUID
-newRandomUUID =
-  do x1 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
-     x2 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
-     x3 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
-     x4 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
-     x5 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
-     x6 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
-     x7 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
-     x8 <- getStdRandom $ randomR (0, (16 ^ 2 - 1))
-     let f = showHexWithLeadingZeros 4 x1 .
-             showHexWithLeadingZeros 4 x2 .
-             showChar '-' .
-             showHexWithLeadingZeros 4 x3 .
-             showChar '-' .
-             showHexWithLeadingZeros 4 x4 .
-             showChar '-' .
-             showHexWithLeadingZeros 4 x5 .
-             showChar '-' .
-             showHexWithLeadingZeros 4 x6 .
-             showHexWithLeadingZeros 4 x7 .
-             showHexWithLeadingZeros 2 x8
-     return (f [])
+newRandomUUID = getStdRandom $ randomR (0, maxBound)
+
+-- -- | Generate an unique identifier.
+-- newRandomUUID :: IO UUID
+-- newRandomUUID =
+--   do x1 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
+--      x2 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
+--      x3 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
+--      x4 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
+--      x5 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
+--      x6 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
+--      x7 <- getStdRandom $ randomR (0, (16 ^ 4 - 1))
+--      x8 <- getStdRandom $ randomR (0, (16 ^ 2 - 1))
+--      let f = showHexWithLeadingZeros 4 x1 .
+--              showHexWithLeadingZeros 4 x2 .
+--              showChar '-' .
+--              showHexWithLeadingZeros 4 x3 .
+--              showChar '-' .
+--              showHexWithLeadingZeros 4 x4 .
+--              showChar '-' .
+--              showHexWithLeadingZeros 4 x5 .
+--              showChar '-' .
+--              showHexWithLeadingZeros 4 x6 .
+--              showHexWithLeadingZeros 4 x7 .
+--              showHexWithLeadingZeros 2 x8
+--      return (f [])
 
 -- | Show the hexadecimal number with leading zeros if required.
 showHexWithLeadingZeros :: Int -> Int -> ShowS
